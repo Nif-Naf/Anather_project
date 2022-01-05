f4df295d5fa0c0193e63aaf1528a4df7f8069bd9
Nikita <Nif-Naf94@yandex.ru>
2022-01-06 00:44:47 +0300

    тша

--- SberFights/One_exemple/prize.py
+++ SberFights/One_exemple/prize.py
@@ -1,28 +1,33 @@
 def get_result(cash, s):
     
     while True:
-        cash.sort()
-        #c = cash[1] + 3
-        
-        cash.insert(1, cash[0] + 3)
-        cash.remove(cash[0])
-        s -= 3
+        """Основной бесконечный цикл."""
+
+        # Сам смысл заменить самый маленький элемент на его сумму с числом 3. 
+        # Подставляем вперед него сумму а сам первый элемент просто удаляем.
+
+        cash.sort()                     # Сортируем список.
+        cash.insert(1, cash[0] + 3)     # Вставляем 2 элементом сумму 1-го элемента и 3.
+        cash.remove(cash[0])            # Удаляем первый элемент списка. 
+        s -= 3                          # Отнимаем у переменной s 3.
+
 
         if s < 3:
-            cash.sort()
-            cash.insert(1, cash[0] + s)
-            cash.remove(cash[0])
-            break
+            """Условие прерывание цикла: когда s будет меньше 3."""
+            cash.sort()                          # Сортируем список.
+            cash.insert(1, cash[0] + s)          # Вставляем 2 элементом сумму 1-го элемента и того что осталось от s.
+            cash.remove(cash[0])                 # Удаляем первый элемент списка.
+            break                                # Выходим из цикла.
 
-    cash.sort()
-    #cash.pop()
-    v = max(cash)
-    b = cash.count(v)
+    cash.sort()                                  # Сортируем список.
+    max_elem = max(cash)                         # Ищем самый большой элемент списка.
+    how_max_elem = cash.count(max_elem)          # Определяем сколько таких больших элементов в спике.
 
-    for i in range(b):
-        cash.remove(v)
+    for i in range(how_max_elem):
+        """В этом цикле удаляем самые большие элементы."""
+        cash.remove(max_elem)                    # Удаляем максимальные элементы.
 
-    return len(cash)
+    return len(cash)                             # Возвращаем длину списка функции.
 
 
 cash = [51, 48, 54, 48]
